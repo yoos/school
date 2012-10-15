9a4a072bd3c9fdc3b7137916c7856a99110dcdce
Soo-Hyun Yoo <yoos117@gmail.com>
2012-10-15 15:33:18 -0700

    Initial commit.

---
+++ myar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
+
