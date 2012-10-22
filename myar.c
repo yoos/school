3d7abfc1282e2d670b6ed60dd6308c5d0531b023
Soo-Hyun Yoo <yoos117@gmail.com>
2012-10-22 11:45:08 -0700

    Add some (undefined) functions we might need.

--- myar.c
+++ myar.c
@@ -5,6 +5,32 @@
 #include <fcntl.h>
 #include <errno.h>
 
+
+// Append file to archive.
+void append() {
+}
+
+// Get a file in the archive.
+void get() {
+}
+
+// Delete file from archive.
+void delete() {
+}
+
+// Extract file from archive.
+void extract() {
+}
+
+// Print a table of contents.
+void toc() {
+}
+
+// Print usage guide.
+void usage() {
+}
+
+
 int main(int argc, char **argv)
 {
 
