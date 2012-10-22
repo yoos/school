b7e429ed6b912954fd00ee91a1d05766455f854e
Soo-Hyun Yoo <yoos117@gmail.com>
2012-10-22 16:14:40 -0700

    Add switch statement for key.

--- myar.c
+++ myar.c
@@ -39,6 +39,45 @@ void usage() {
 
 int main(int argc, char **argv)
 {
+	/* Has the user provided enough arguments? */
+	if (argc < 3) {
+		usage();
+	} else {
+		switch (argv[1]) {
+
+		/* Quickly append named files to archive. */
+		case 'q':
+			break;
+
+		/* Extract named files. */
+		case 'x':
+			break;
+
+		/* Print concise table of contents of archive. */
+		case 't':
+			break;
+
+		/* Print verbose table of contents of archive. */
+		case 'v':
+			break;
+
+		/* Delete named files from archive. */
+		case 'd':
+			break;
+
+		/* Quickly append all "regular" files in current directory, except the
+		 * archive itself. */
+		case 'A':
+			break;
+
+		/* For a given timeout, add all modified files to the archive. */
+		case 'w':
+			break;
+
+		/* Print usage guide if used incorrectly. */
+		default:
+			usage();
+		}
 
 	return 0;
 }
