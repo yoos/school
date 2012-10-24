c3a7bf90461859b64a3e4bf59c7eac46c3bdea71
Soo-Hyun Yoo <yoos117@gmail.com>
2012-10-23 18:39:39 -0700

    Program builds.

--- myar.c
+++ myar.c
@@ -1,29 +1,46 @@
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <ar.h>
 
+
+#define BLOCKSIZE 4096
 
 /** Append file to archive.
  */
-void append() {
+void append(int fd, char* name) {
+	int i;
+	uint8_t buffer[16];
+	lseek(fd, 0*BLOCKSIZE, SEEK_SET);
+
+	/* Check that this is an archive file. */
+	if (read(fd, buffer, SARMAG) > 0) {
+		for (i=0; i<SARMAG; i++) {
+			if (ARMAG[i] != buffer[i]) {
+				perror("This is not an archive file!");
+				exit(1);
+			}
+		}
+	}
 }
 
 /** Get a file in the archive.
  */
-void get() {
+void get(int fd, char* name) {
 }
 
 /** Delete file from archive.
  */
-void delete() {
+void delete(int fd, char* name) {
 }
 
 /** Extract file from archive.
  */
-void extract() {
+void extract(int fd, char* name) {
 }
 
 /** Print a table of contents.
@@ -39,18 +56,33 @@ void usage() {
 
 int main(int argc, char **argv)
 {
+	int fd, i;
+
 	/* Has the user provided enough arguments? */
 	if (argc < 3) {
 		usage();
 	} else {
-		switch (argv[1]) {
+		/* Open file. */
+		fd = open(argv[2], O_WRONLY | O_CREAT | S_IRWXU);
+		if (fd == -1) {
+			perror("Could not open file!");
+			exit(-1);
+		}
+
+		switch (*argv[1]) {
 
 		/* Quickly append named files to archive. */
 		case 'q':
+			for (i=3; i<argc; i++) {
+				append(fd, argv[i]);
+			}
 			break;
 
 		/* Extract named files. */
 		case 'x':
+			for (i=3; i<argc; i++) {
+				extract(fd, argv[i]);
+			}
 			break;
 
 		/* Print concise table of contents of archive. */
@@ -63,6 +95,9 @@ int main(int argc, char **argv)
 
 		/* Delete named files from archive. */
 		case 'd':
+			for (i=3; i<argc; i++) {
+				delete(fd, argv[i]);
+			}
 			break;
 
 		/* Quickly append all "regular" files in current directory, except the
@@ -78,6 +113,7 @@ int main(int argc, char **argv)
 		default:
 			usage();
 		}
+	}
 
 	return 0;
 }
