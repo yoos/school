3176ddbdf6b81d18ced89c8d7beb00f2ec2460df
Soo-Hyun Yoo <yoos117@gmail.com>
2012-11-04 02:11:54 -0800

    Fork children.

--- cs311/hw4/uniquify.c
+++ cs311/hw4/uniquify.c
@@ -19,10 +19,23 @@ int main(int argc, char **argv)
 	if (argc != 2) {
 		// usage();
 	} else {
-		int i;
-		for (i=0; i<atoi(argv[1]); i++) {
-			// Fork off multiple sort processes
-			printf("Fork %d\n", i);
+		int parentPID = getpid();
+		int procNum = atoi(argv[1]);   // Number of sort processes to spawn.
+
+		FILE sort_fd[procNum];
+
+		// Fork off sort processes.
+		int i=0;
+		while (getpid() == parentPID && i<procNum) {
+			if (fork()) {
+				// Do parent stuff
+				printf("Forked child %d\n", i);
+			} else {
+				// Do child stuff
+				printf("Child %d born!\n", i);
+			}
+
+			i++;
 		}
 
 		// Parse input
