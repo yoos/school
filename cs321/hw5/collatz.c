5cb821e3b33a7be452bc8b19c9e2e82a93a2eaa3
Soo-Hyun Yoo <yoos117@gmail.com>
2012-11-05 01:10:32 -0800

    Add CS321 HW 5 stuff.

---
+++ cs321/hw5/collatz.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+
+int collatz(int x)
+{
+	if (x == 1) {
+		return 1;
+	} else if (x % 2 == 0) {
+		return x/2;
+	} else {
+		return 3*x+1;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		printf("Too few arguments.\n");
+		exit(-1);
+	}
+
+	int x, i;
+	int num;
+
+	for (num=1; num<=atoi(argv[1]); num++) {
+		x = num;
+		i = 0;
+
+		while (x != 1) {
+			x = collatz(x);
+			i++;
+		}
+
+		printf("%4d: %d\n", num, i);
+	}
+
+	return 0;
+}
+
