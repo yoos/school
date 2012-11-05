cb469689bcedde9dcf0f0e905c66d0ce46a9334d
Soo-Hyun Yoo <yoos117@gmail.com>
2012-11-05 13:38:33 -0800

    Finished assignment.

--- cs321/hw5/collatz.c
+++ cs321/hw5/collatz.c
@@ -5,13 +5,9 @@
 
 int collatz(int x)
 {
-	if (x == 1) {
-		return 1;
-	} else if (x % 2 == 0) {
-		return x/2;
-	} else {
-		return 3*x+1;
-	}
+	if (x == 1)     return 1;
+	if (x % 2 == 0) return x/2;
+	else            return 3*x+1;
 }
 
 int main(int argc, char **argv)
