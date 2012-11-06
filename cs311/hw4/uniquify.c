134d1f994520abbaf8a6b209db45771ff9b78780
Soo-Hyun Yoo <yoos117@gmail.com>
2012-11-05 23:09:04 -0800

    Remove obsolete comments and old code.

--- cs311/hw4/uniquify.c
+++ cs311/hw4/uniquify.c
@@ -39,8 +39,6 @@ int main(int argc, char **argv)
 			pipe(fdFmSort[i]);
 
 			if (fork()) {
-				printf("Forked child %d\n", i);
-
 				// Close unused ends of pipes.
 				close(fdToSort[i][0]);
 				close(fdFmSort[i][1]);
@@ -49,10 +47,6 @@ int main(int argc, char **argv)
 				stToSort[i][1] = fdopen(fdToSort[i][1], "w");
 				stFmSort[i][0] = fdopen(fdFmSort[i][0], "r");
 			} else {
-				printf("Child %d born!\n", i);
-
-				//sleep(1);
-
 				// Close unused ends of pipes.
 				close(fdToSort[i][1]);
 				close(fdFmSort[i][0]);
@@ -69,48 +63,11 @@ int main(int argc, char **argv)
 				stToSort[i][0] = fdopen(fdToSort[i][0], "r");
 				stFmSort[i][1] = fdopen(fdFmSort[i][1], "w");
 
-				//while (1) {
-				//	sprintf(buf, "Hello from child %d\n", i);
-				//	if (fputs(buf, stFmSort[i][1]) == EOF) {
-				//		perror("Child failed to write");
-				//		exit(-1);
-				//	}
-				//	fflush(stFmSort[i][1]);
-
-				//	if (fgets(buf, 20, stToSort[i][0]) == NULL) {
-				//		perror("Child failed to read");
-				//		exit(-1);
-				//	}
-				//	printf("Child %d read: %s\n", i, buf);
-				//}
-				//fclose(stToSort[i][0]);
-				//printf("Child exiting.\n");
-
 				execlp("sort", "sort", NULL);
 			}
 
 			i++;
 		}
-
-		//int j;
-		//for (j=10; j>0; j--) {
-		//	sprintf(buf, "Hello from parent %d\n", j);
-		//	if (fputs(buf, stToSort[0][1]) == EOF) {
-		//		perror("Parent failed to write");
-		//		exit(-1);
-		//	}
-		//	fflush(stToSort[0][1]);
-		//}
-		//fclose(stToSort[0][1]);
-
-		//while (1) {
-		//	if (fgets(buf, 256, stFmSort[0][0]) == NULL) {
-		//		perror("Parent failed to read");
-		//		exit(-1);
-		//	}
-		//	printf("Parent read: %s\n", buf);
-		//}
-		//printf("Parent exiting.\n");
 
 		// Parse input
 		long unsigned int parseCount = parse(stdin, stToSort, procNum);
@@ -120,18 +77,11 @@ int main(int argc, char **argv)
 			fclose(stToSort[i][1]);
 		}
 
-		//for (i=0; i<procNum; i++) {
-		//	printf("Read from child %d:\n", i);
-		//	while (fgets(buf, 256, stFmSort[i][0]) != NULL) {
-		//		printf("%s\n", buf);
-		//	}
-		//}
-
 		// Merge results and print
 		long unsigned int mergeCount = merge(stdout, stFmSort, procNum);
 
 		// Print results.
-		printf("\n\n%lu lines parsed.\n", parseCount);
+		printf("\n%lu lines parsed.\n", parseCount);
 		printf("%lu lines printed.\n", mergeCount);
 	}
 
