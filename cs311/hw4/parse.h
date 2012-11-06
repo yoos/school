f1a1f0bc7231526586d81313f265a2c0b0821f46
Soo-Hyun Yoo <yoos117@gmail.com>
2012-11-05 22:25:36 -0800

    Add comments.

--- cs311/hw4/parse.h
+++ cs311/hw4/parse.h
@@ -1,19 +1,23 @@
 
+// Parse inputs and hand out to sort processes round-robin style. Return number
+// of entries parsed.
 unsigned long parse(FILE *input, FILE *stToSort[][2], int procNum)
 {
 	char str[256];
 	unsigned long count = 0;
 	int proc = 0;   // Process number
 
-	// Hand out entries to sort processes, round-robin style.
 	while (fgets(str, 256, input) != NULL) {
+		// Feed word to sort process
 		if (fputs(str, stToSort[proc][1]) == EOF) {
 			perror("Failed to feed sort");
 			exit(-1);
 		}
 		fflush(stToSort[proc][1]);
 
+		// Calculate index of next process
 		proc = (proc+1) % procNum;
+
 		count++;
 	}
 
