75464a07438f185f270411ba881b18cded162214
Soo-Hyun Yoo <yoos117@gmail.com>
2012-11-05 23:22:14 -0800

    Generate duplicate word list.

--- cs311/hw4/randgen.py
+++ cs311/hw4/randgen.py
@@ -1,6 +1,16 @@
 import random
 import sys
 
-for i in range(int(sys.argv[1])):
-	print ''.join([random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") for j in range(random.randint(1,256))])
+if (sys.argv[1] == 'u'):
+	for i in range(int(sys.argv[2])):
+		print ''.join([random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") for j in range(random.randint(1,256))])
+else:
+	count = 0
+	while (count < 1000):
+		word = ''.join([random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") for j in range(random.randint(1,30))])
+		for i in range(random.randint(1,20)):
+			print word
+			count += 1
+			if (count == 1000):
+				break
 
