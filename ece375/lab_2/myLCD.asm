671ce861d8004ad6c1803eec6d543d16b49e62f7
Soo-Hyun Yoo <yoos117@gmail.com>
2013-01-16 18:32:24 -0800

    Works!

--- ece375/lab_2/myLCD.asm
+++ ece375/lab_2/myLCD.asm
@@ -19,6 +19,7 @@
 ;*	Internal Register Definitions and Constants
 ;***********************************************************
 .def	mpr = r16				; Multipurpose register required for LCD Driver
+.def	ReadCnt = r23			; Counter used to read data from program memory
 
 
 ;***********************************************************
@@ -39,25 +40,54 @@
 ;-----------------------------------------------------------
 INIT:							; The initialization routine
 		; Initialize Stack Pointer
-								; Init the 2 stack pointer registers
+		ldi		mpr, HIGH(RAMEND)
+		out		SPH, mpr
+		ldi		mpr, LOW(RAMEND)
+		out		SPL, mpr
 
 		; Initialize LCD Display
-								; An RCALL statement
+		rcall	LCDInit
 
 		; Move strings from Program Memory to Data Memory
-								; A while loop will go here
-
-		; NOTE that there is no RET or RJMP from INIT, this is
-		; because the next instruction executed is the first for
-		; the main program
+		ldi		ZL,  low(TXT0<<1)
+		ldi		ZH, high(TXT0<<1)
+		ldi		YL,  low(LCDLn1Addr)
+		ldi		YH, high(LCDLn1Addr)
+		ldi		ReadCnt, LCDMaxCnt
+INIT_LINE1:
+		lpm		mpr, Z+			; Read program memory
+		st		Y+, mpr			; Store into data memory
+		dec		ReadCnt			; Decrement counter
+		brne	INIT_LINE1		; Loop
 
 ;-----------------------------------------------------------
 ; Main Program
 ;-----------------------------------------------------------
 MAIN:							; The Main program
-		; Display the strings on the LCD Display
-								; An RCALL statement
-		
+		ldi		ZL,  low(TXT0<<1)
+		ldi		ZH, high(TXT0<<1)
+		ldi		YL,  low(LCDLn1Addr)
+		ldi		YH, high(LCDLn1Addr)
+		ldi		ReadCnt, LCDMaxCnt
+PRINT_LINE_1:
+		lpm		mpr, Z+
+		st		Y+, mpr
+		dec		ReadCnt
+		brne	PRINT_LINE_1
+		rcall	LCDWrLn1
+
+		ldi		ZL,  low(TXT1<<1)
+		ldi		ZH, high(TXT1<<1)
+		ldi		YL,  low(LCDLn2Addr)
+		ldi		YH, high(LCDLn2Addr)
+		ldi		ReadCnt, LCDMaxCnt
+PRINT_LINE_2:
+		lpm		mpr, Z+
+		st		Y+, mpr
+		dec		ReadCnt
+		brne	PRINT_LINE_2
+		rcall	LCDWrLn2
+
 		rjmp	MAIN			; jump back to main and create an infinite
 								; while loop.  Generally, every main program is an
 								; infinite while loop, never let the main program
@@ -67,18 +97,32 @@ MAIN:							; The Main program
 ;*	Functions and Subroutines
 ;***********************************************************
 
-;-----------------------------------------------------------
-; Func: Template function header
-; Desc: Cut and paste this and fill in the info at the 
-;		beginning of your functions
-;-----------------------------------------------------------
-FUNC:							; Begin a function with a label
-		; Save variable by pushing them to the stack
+;***********************************************************
+;* Func:	WriteText
+;* Desc:	Writes the text that is pointed to by the Z pointer
+;*			from Program Memory to the second line of the LCD 
+;*			Display.
+;***********************************************************
+WriteText:
+		push	mpr				; Save the mpr register
+		push	ReadCnt			; Save the ReadCounter
+		rcall	LCDClrLn2		; CLEAR LINE 2 OF LCD
+								; LOAD THE LCD MAX LINE COUNT (16)
+		ldi		ReadCnt, LCDMaxCnt
+								; LOAD THE Y POINTER WITH THE DATA
+								; ADDRESS FOR LINE 2 DATA
+		ldi		YL, low(LCDLn2Addr)
+		ldi		YH, high(LCDLn2Addr)
+WriteText_lp:					; Loop that reads the data
+		lpm		mpr, Z+			; Read program data
+		st		Y+, mpr			; Store data to memory
+		dec		ReadCnt			; Decrement counter
+		brne	WriteText_lp	; Loop untill all data is read
+		rcall	LCDWrLn2		; WRITE DATA TO LINE 2
+		pop		ReadCnt			; Restore the ReadCounter
+		pop		mpr				; Restore the mpr register
+		ret						; Return from function
 
-		; Execute the function here
-		
-		; Restore variable by popping them from the stack in reverse order\
-		ret						; End a function with RET
 
 
 ;***********************************************************
@@ -89,9 +133,10 @@ FUNC:							; Begin a function with a label
 ; An example of storing a string, note the preceeding and
 ; appending labels, these help to access the data
 ;----------------------------------------------------------
-STRING_BEG:
-.DB		"My Test String"		; Storing the string in Program Memory
-STRING_END:
+TXT0:
+.DB		"Soo-Hyun Yoo    "		; Storing the string in Program Memory
+TXT1:
+.DB		"Hello world!    "
 
 ;***********************************************************
 ;*	Additional Program Includes
