b0b3d702ce8b170be31dc30bd37bb1c81fdd3847
Soo-Hyun Yoo <yoos117@gmail.com>
2013-01-16 18:35:08 -0800

    Remove unused code.

--- ece375/lab_2/myLCD.asm
+++ ece375/lab_2/myLCD.asm
@@ -49,10 +49,6 @@ INIT:							; The initialization routine
 		rcall	LCDInit
 
 		; Move strings from Program Memory to Data Memory
-		ldi		ZL,  low(TXT0<<1)
-		ldi		ZH, high(TXT0<<1)
-		ldi		YL,  low(LCDLn1Addr)
-		ldi		YH, high(LCDLn1Addr)
 		ldi		ReadCnt, LCDMaxCnt
 INIT_LINE1:
 		lpm		mpr, Z+			; Read program memory
@@ -64,6 +60,7 @@ INIT_LINE1:
 ; Main Program
 ;-----------------------------------------------------------
 MAIN:							; The Main program
+		; Write first line
 		ldi		ZL,  low(TXT0<<1)
 		ldi		ZH, high(TXT0<<1)
 		ldi		YL,  low(LCDLn1Addr)
@@ -76,6 +73,7 @@ PRINT_LINE_1:
 		brne	PRINT_LINE_1
 		rcall	LCDWrLn1
 
+		; Write second line
 		ldi		ZL,  low(TXT1<<1)
 		ldi		ZH, high(TXT1<<1)
 		ldi		YL,  low(LCDLn2Addr)
@@ -92,37 +90,6 @@ PRINT_LINE_2:
 								; while loop.  Generally, every main program is an
 								; infinite while loop, never let the main program
 								; just run off
-
-;***********************************************************
-;*	Functions and Subroutines
-;***********************************************************
-
-;***********************************************************
-;* Func:	WriteText
-;* Desc:	Writes the text that is pointed to by the Z pointer
-;*			from Program Memory to the second line of the LCD 
-;*			Display.
-;***********************************************************
-WriteText:
-		push	mpr				; Save the mpr register
-		push	ReadCnt			; Save the ReadCounter
-		rcall	LCDClrLn2		; CLEAR LINE 2 OF LCD
-								; LOAD THE LCD MAX LINE COUNT (16)
-		ldi		ReadCnt, LCDMaxCnt
-								; LOAD THE Y POINTER WITH THE DATA
-								; ADDRESS FOR LINE 2 DATA
-		ldi		YL, low(LCDLn2Addr)
-		ldi		YH, high(LCDLn2Addr)
-WriteText_lp:					; Loop that reads the data
-		lpm		mpr, Z+			; Read program data
-		st		Y+, mpr			; Store data to memory
-		dec		ReadCnt			; Decrement counter
-		brne	WriteText_lp	; Loop untill all data is read
-		rcall	LCDWrLn2		; WRITE DATA TO LINE 2
-		pop		ReadCnt			; Restore the ReadCounter
-		pop		mpr				; Restore the mpr register
-		ret						; Return from function
-
 
 
 ;***********************************************************
