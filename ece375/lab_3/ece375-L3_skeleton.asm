cca9f3c06e76920947ec0566a76d1e51f03c40a7
Soo-Hyun Yoo <yoos117@gmail.com>
2013-01-29 23:20:50 -0800

    Copy MUL16 code to MUL24.

--- ece375/lab_3/ece375-L3_skeleton.asm
+++ ece375/lab_3/ece375-L3_skeleton.asm
@@ -131,12 +131,78 @@ ADD16:
 ;		result.
 ;-----------------------------------------------------------
 MUL24:
-		; Save variable by pushing them to the stack
+		push 	A				; Save A register
+		push	B				; Save B register
+		push	rhi				; Save rhi register
+		push	rlo				; Save rlo register
+		push	zero			; Save zero register
+		push	XH				; Save X-ptr
+		push	XL
+		push	YH				; Save Y-ptr
+		push	YL				
+		push	ZH				; Save Z-ptr
+		push	ZL
+		push	oloop			; Save counters
+		push	iloop				
 
-		; Execute the function here
-		
-		; Restore variable by popping them from the stack in reverse order\
+		clr		zero			; Maintain zero semantics
+
+		; Set Y to beginning address of B
+		ldi		YL, low(addrB)	; Load low byte
+		ldi		YH, high(addrB)	; Load high byte
+
+		; Set Z to begginning address of resulting Product
+		ldi		ZL, low(LAddrP)	; Load low byte
+		ldi		ZH, high(LAddrP); Load high byte
+
+		; Begin outer for loop
+		ldi		oloop, 2		; Load counter
+MUL16_OLOOP:
+		; Set X to beginning address of A
+		ldi		XL, low(addrA)	; Load low byte
+		ldi		XH, high(addrA)	; Load high byte
+
+		; Begin inner for loop
+		ldi		iloop, 2		; Load counter
+MUL16_ILOOP:
+		ld		A, X+			; Get byte of A operand
+		ld		B, Y			; Get byte of B operand
+		mul		A,B				; Multiply A and B
+		ld		A, Z+			; Get a result byte from memory
+		ld		B, Z+			; Get the next result byte from memory
+		add		rlo, A			; rlo <= rlo + A
+		adc		rhi, B			; rhi <= rhi + B + carry
+		ld		A, Z			; Get a third byte from the result
+		adc		A, zero			; Add carry to A
+		st		Z, A			; Store third byte to memory
+		st		-Z, rhi			; Store second byte to memory
+		st		-Z, rlo			; Store third byte to memory
+		adiw	ZH:ZL, 1		; Z <= Z + 1			
+		dec		iloop			; Decrement counter
+		brne	MUL16_ILOOP		; Loop if iLoop != 0
+		; End inner for loop
+
+		sbiw	ZH:ZL, 1		; Z <= Z - 1
+		adiw	YH:YL, 1		; Y <= Y + 1
+		dec		oloop			; Decrement counter
+		brne	MUL16_OLOOP		; Loop if oLoop != 0
+		; End outer for loop
+		 		
+		pop		iloop			; Restore all registers in reverves order
+		pop		oloop
+		pop		ZL				
+		pop		ZH
+		pop		YL
+		pop		YH
+		pop		XL
+		pop		XH
+		pop		zero
+		pop		rlo
+		pop		rhi
+		pop		B
+		pop		A
 		ret						; End a function with RET
+
 
 ;-----------------------------------------------------------
 ; Func: MUL16
