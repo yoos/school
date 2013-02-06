4e803534dd9533db048769b35a483571cd03423d
Soo-Hyun Yoo <yoos117@gmail.com>
2013-02-05 19:19:49 -0800

    Add Lab 4 files.

---
+++ ece375/lab_4/interrupt.asm
@@ -0,0 +1,113 @@
+;***********************************************************
+;*
+;*	Enter Name of file here
+;*
+;*	Enter the description of the program here
+;*
+;*	This is the skeleton file Lab 4 of ECE 375
+;*
+;***********************************************************
+;*
+;*	 Author: Enter your name
+;*	   Date: Enter Date
+;*
+;***********************************************************
+
+.include "m128def.inc"			; Include definition file
+
+;***********************************************************
+;*	Internal Register Definitions and Constants
+;***********************************************************
+.def	mpr = r16				; Multipurpose register 
+; Other register renames
+
+; Constants for interactions such as
+.equ	WskrR = 0				; Right Whisker Input Bit
+.equ	WskrL = 1				; Left Whisker Input Bit
+
+; Using the constants from above, create the movement 
+; commands, Forwards, Backwards, Stop, Turn Left, and Turn Right
+
+;***********************************************************
+;*	Start of Code Segment
+;***********************************************************
+.cseg							; Beginning of code segment
+
+;-----------------------------------------------------------
+; Interrupt Vectors
+;-----------------------------------------------------------
+.org	$0000					; Beginning of IVs
+		rjmp 	INIT			; Reset interrupt
+
+; Set up the interrupt vectors for the interrupts, .i.e
+;.org	$002E					; Analog Comparator IV
+;		rcall	HandleAC		; Function to handle Interupt request
+;		reti					; Return from interrupt
+
+.org	$0046					; End of Interrupt Vectors
+
+;-----------------------------------------------------------
+; Program Initialization
+;-----------------------------------------------------------
+INIT:	; The initialization routine
+		; Initialize Stack Pointer
+
+		; Initialize Port B for output
+
+		; Initialize Port D for input
+
+		; Initialize external interrupts
+		; Set the Interrupt Sense Control to low level 
+		; NOTE: must initialize both EICRA and EICRB
+
+		; Set the External Interrupt Mask
+
+		; Turn on interrupts
+		; NOTE: This must be the last thing to do in the INIT function
+
+;-----------------------------------------------------------
+; Main Program
+;-----------------------------------------------------------
+MAIN:	; The Main program
+
+		; Send command to Move Robot Forward 
+		; That is all you should have in MAIN
+
+		rjmp	MAIN			; Create an infinite while loop to signify the 
+								; end of the program.
+
+;***********************************************************
+;*	Functions and Subroutines
+;***********************************************************
+
+;-----------------------------------------------------------
+; You will probably need several functions, one to handle the 
+; left whisker interrupt, one to handle the right whisker 
+; interrupt, and maybe a wait function
+;------------------------------------------------------------
+
+;-----------------------------------------------------------
+; Func: Template function header
+; Desc: Cut and paste this and fill in the info at the 
+;		beginning of your functions
+;-----------------------------------------------------------
+FUNC:	; Begin a function with a label
+
+		; Save variable by pushing them to the stack
+
+		; Execute the function here
+		
+		; Restore variable by popping them from the stack in reverse order
+
+		ret		; End a function with RET
+
+;***********************************************************
+;*	Stored Program Data
+;***********************************************************
+
+; Enter any stored data you might need here
+
+;***********************************************************
+;*	Additional Program Includes
+;***********************************************************
+; There are no additional file includes for this program
