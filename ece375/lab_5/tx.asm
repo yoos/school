3e8e688421b4e811c6a033c8c20dd057ec929566
Soo-Hyun Yoo <yoos117@gmail.com>
2013-02-19 21:23:48 -0800

    Read buttons.

--- ece375/lab_5/tx.asm
+++ ece375/lab_5/tx.asm
@@ -41,12 +41,13 @@
 ; Use these commands between the remote and TekBot
 ; MSB = 1 thus:
 ; commands are shifted right by one and ORed with 0b10000000 = $80
-.equ	MovFwd =  ($80|1<<(EngDirR-1)|1<<(EngDirL-1))	;0b10110000 Move Forwards Command
-.equ	MovBck =  ($80|$00)								;0b10000000 Move Backwards Command
-.equ	TurnR =   ($80|1<<(EngDirL-1))					;0b10100000 Turn Right Command
-.equ	TurnL =   ($80|1<<(EngDirR-1))					;0b10010000 Turn Left Command
-.equ	Halt =    ($80|1<<(EngEnR-1)|1<<(EngEnL-1))		;0b11001000 Halt Command
-.equ	Freez =   ($80|$F8)
+.equ	MovFwdCmd = ($80|1<<(EngDirR-1)|1<<(EngDirL-1))		;0b10110000 Move Forwards Command
+.equ	MovBckCmd = ($80|$00)								;0b10000000 Move Backwards Command
+.equ	TurnRCmd  = ($80|1<<(EngDirL-1))					;0b10100000 Turn Right Command
+.equ	TurnLCmd  = ($80|1<<(EngDirR-1))					;0b10010000 Turn Left Command
+.equ	HaltCmd   = ($80|1<<(EngEnR-1)|1<<(EngEnL-1))		;0b11001000 Halt Command
+.equ	FrzCmd    = ($80|$F8)
+
 ;***********************************************************
 ;*	Start of Code Segment
 ;***********************************************************
@@ -58,6 +59,9 @@
 .org	$0000					; Beginning of IVs
 		rjmp 	INIT			; Reset interrupt
 
+.org	$0002
+		rjmp	TX
+		reti
 
 .org	$0046					; End of Interrupt Vectors
 
@@ -104,13 +108,69 @@ INIT:
 ; Main Program
 ;-----------------------------------------------------------
 MAIN:
+		in		mpr, PIND;
+		andi	mpr, (1<<FwdBtn)|(1<<BckBtn)|(1<<TurnRBtn)|(1<<TurnLBtn)|(1<<HaltBtn)|(1<<FrzBtn)
 
+		; Check for forward button press
+		cpi		mpr, (1<<FwdBtn)
+		brne	BCK
+		ldi		mpr, MovFwdCmd
+		rcall	TX
 		rjmp	MAIN
+
+BCK:	; Check for back button press
+		cpi		mpr, (1<<BckBtn)
+		brne	TURNR
+		ldi		mpr, MovBckCmd
+		rcall	TX
+		rjmp	MAIN
+
+TURNR:	; Check for turn right button press
+		cpi		mpr, (1<<TurnRBtn)
+		brne	TURNL
+		ldi		mpr, TurnRCmd
+		rcall	TX
+		rjmp	MAIN
+
+TURNL:	; Check for turn left button press
+		cpi		mpr, (1<<TurnLBtn)
+		brne	HALT
+		ldi		mpr, TurnLCmd
+		rcall	TX
+		rjmp	MAIN
+
+HALT:	; Check for halt button press
+		cpi		mpr, (1<<HaltBtn)
+		brne	FRZ
+		ldi		mpr, HaltCmd
+		rcall	TX
+		rjmp	MAIN
+
+FRZ:	; Check for freeze button press
+		cpi		mpr, (1<<FrzBtn)
+		brne	MAIN
+		ldi		mpr, FrzCmd
+		rcall	TX
+		rjmp	MAIN
+
 
 ;***********************************************************
 ;*	Functions and Subroutines
 ;***********************************************************
+TX:
+		push	mpr				; Save mpr register
+		push	waitcnt			; Save wait register
+		in		mpr, SREG		; Save program state
+		push	mpr
 
+		; INSERT UART CODE HERE
+
+		pop		mpr
+		out		SREG, mpr
+		pop		waitcnt
+		pop		mpr
+
+		ret
 
 
 ;***********************************************************
