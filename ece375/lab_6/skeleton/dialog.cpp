6c067a28da02652a427522644dce2431bc038c26
Soo-Hyun Yoo <yoos117@gmail.com>
2013-03-07 18:51:29 -0800

    Works!

--- ece375/lab_6/skeleton/dialog.cpp
+++ ece375/lab_6/skeleton/dialog.cpp
@@ -15,9 +15,10 @@ int file;
 int adapter_nr = 14;	//dynamically determined; in our system, i2cdetect -l shows i2c0 is for smbus (SMBus I801 adapter at 0400)
 char filename[20];
 
-int datal[6];
+int datal[6];   // Raw sensor reads
+float dataf[5];   // Sensor reads mapped to [-1, 1]
 int z=0,c;
-float a=0,b=300,p=0,q=300;
+int xa=0, xb=0, ya=0, yb=0;
 int k=1;
 
 QPen pen;
@@ -51,11 +52,17 @@ void openfile()
         //if it returns with error (-1) exit with status 1\
 
     // Initialize white nunchuck
-    if(i2c_smbus_write_byte(file, 0x40) == -1) {
-        printf("error..., something is wrong  gvcn jhbv %s\n", strerror(errno));
-        exit(1);
-    }
-    if(i2c_smbus_write_byte(file, 0x00) == -1) {
+//    if(i2c_smbus_write_byte(file, 0x40) == -1) {
+//        printf("error..., something is wrong  gvcn jhbv %s\n", strerror(errno));
+//        exit(1);
+//    }
+//    if(i2c_smbus_write_byte(file, 0x00) == -1) {
+//        printf("error..., something is wrong %s\n", strerror(errno));
+//        exit(1);
+//    }
+
+    if(i2c_smbus_write_byte_data(file, 0x40, 0x00) == -1)
+    {
         printf("error..., something is wrong %s\n", strerror(errno));
         exit(1);
     }
@@ -105,7 +112,7 @@ void Dialog::paintEvent(QPaintEvent *e)
                 data[i] = (unsigned char) i2c_smbus_read_byte(file);
 						//TODO : convert the input data to the correct format
                 data[i] = (data[i] ^ 0x17) + 0x17;
-                printf("the final data is %x\n", data[i]);
+                //printf("the final data is %x\n", data[i]);
 
             }
 
@@ -116,8 +123,6 @@ void Dialog::paintEvent(QPaintEvent *e)
         datal[3] = (data[3] << 2) | (data[5] & 0x30);
         datal[4] = (data[4] << 2) | (data[5] & 0xc0);
 
-        //        dataf[0] = (((data[0])-0x1e) * 2.0)/(0xe1-0x1e) - 1.0;
-        //  dataf[1] = (((data[1])-0x1d) * 2.0)/(0xdf-0x1d) - 1.0;
         z = (data[5] & 0x01);
         c = (data[5] & 0x02) >> 1;
 
@@ -126,26 +131,52 @@ void Dialog::paintEvent(QPaintEvent *e)
 				//TODO: Print output of analog stick and X, Y, Z axes
         printf("Analog X: %d\n", datal[0]);
         printf("Analog Y: %d\n", datal[1]);
-        printf("Accel X: %d\n", datal[2]);
-        printf("Accel Y: %d\n", datal[3]);
-        printf("Accel Z: %d\n", datal[4]);
+//        printf("Accel X: %d\n", datal[2]);
+//        printf("Accel Y: %d\n", datal[3]);
+//        printf("Accel Z: %d\n", datal[4]);
 
 				//TODO: Print output of buttons Z and C
-        printf("Button Z: %d\n", z);
-        printf("Button C: %d\n", c);
-				//When drawing pressing button Z should exit
-        //if(z) exit(0);
+//        printf("Button Z: %d\n", z);
+//        printf("Button C: %d\n", c);
 
-				//When drawing pressing button C should draw
-        if(!c) return;
 
 		//Calculate the next point based on analog stick data.	
 		//You might need to convert some values.  Experiment...
+        dataf[0] = (((data[0])-23) * 2.0)/(217-23) - 1.0;
+        dataf[1] = (((data[1])-32) * 2.0)/(235-32) - 1.0;
 
+        //printf("AX: %f\n", dataf[0]);
+        //printf("AY: %f\n", dataf[1]);
 
 		//Draw a line between the previous point and the new point.
+        xa = xb;
+        ya = yb;
+        xb += (int) (30*dataf[0]);
+        yb -= (int) (30*dataf[1]);
+        xb = (xb < 0) ? 0 : xb;
+        xb = (xb > 300) ? 300 : xb;
+        yb = (yb < 0) ? 0 : yb;
+        yb = (yb > 300) ? 300 : yb;
 
-    std::cout<<a<<" "<<b<<"\n";
+        //When drawing pressing button C should draw
+if(!c) {
+    pen.setColor(QColor::fromRgb(255,0,0));
+    painter.setPen(pen);
+}
+
+        printf("AX: %f  AY: %f  +x: %d  +y: %d\n", dataf[0], dataf[1], (int) (5*dataf[0]), (int) (5*dataf[1]));
+
+        //printf("%d %d %d %d\n", xa, ya, xb, yb);
+
+        painter.drawLine(xa, ya, xb, yb);
+
+        //When drawing pressing button Z should exit
+if(!z) {
+    painter.eraseRect(0,0, this->width(), this->height());
+}
+
+
+    //std::cout<<a<<" "<<b<<"\n";
 
 }
 
