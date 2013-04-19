3c99738ab5b285ccd79fa84f49c3f7e32a489ef8
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-18 19:11:00 -0700

    Action function should return empty list of index is invalid.

--- cs331/pa1/actions.py
+++ cs331/pa1/actions.py
@@ -17,7 +17,7 @@ def act(gs, actNum):
     curBank, endBank = getBanks(gs)
 
     # Move people. These are numbered according to the order specified in the assignment.
-    if actNum == 1:
+    if actNum == 0:
         # Put one missionary in the boat
         if succ[curBank][0] >= 1:
             succ[curBank][0] -= 1
@@ -25,7 +25,7 @@ def act(gs, actNum):
         else:
             return []
 
-    elif actNum == 2:
+    elif actNum == 1:
         # Put two missionaries in the boat
         if succ[curBank][0] >= 2:
             succ[curBank][0] -= 2
@@ -33,7 +33,7 @@ def act(gs, actNum):
         else:
             return []
 
-    elif actNum == 3:
+    elif actNum == 2:
         # Put one cannibal in the boat
         if succ[curBank][1] >= 1:
             succ[curBank][1] -= 1
@@ -41,7 +41,7 @@ def act(gs, actNum):
         else:
             return []
 
-    elif actNum == 4:
+    elif actNum == 3:
         # Put one cannibal and one missionary in the boat
         if succ[curBank][0] >= 1 and succ[curBank][1] >= 1:
             succ[curBank][0] -= 1
@@ -51,13 +51,16 @@ def act(gs, actNum):
         else:
             return []
 
-    elif actNum == 5:
+    elif actNum == 4:
         # Put two cannibals in the boat
         if succ[curBank][1] >= 2:
             succ[curBank][1] -= 2
             succ[endBank][1] += 2
         else:
             return []
+
+    else:
+        return []
 
     # Move boat to other side.
     moveBoat(succ)
