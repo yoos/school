b5a95b52027570658459a2f2391cbfb23cabfc86
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-23 01:30:16 -0700

    Implement half adder.

--- cs381/hw1/DiCiDL.hs
+++ cs381/hw1/DiCiDL.hs
@@ -1,9 +1,9 @@
 module DiCiDL where
 
-data Circuit = Mult Gates Links
+data Circuit = Circ Gates Links
              deriving Show
 
-data Gates = GateNum Int GateFn Gates
+data Gates = Gate Int GateFn Gates
            | GatesE
            deriving Show
 
@@ -14,6 +14,11 @@ data Links = Link (Int, Int) (Int, Int) Links
            | LinksE
            deriving Show
 
+
+halfAdder = Circ (Gate 1 XOR
+                 (Gate 2 AND GatesE))
+                 (Link (1, 1) (2, 1)
+                 (Link (1, 2) (2, 2) LinksE))
 
 -- vim: expandtab
 
