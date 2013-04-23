1cdb977f7a2db77c9a3cb7c025606e17b90f8db8
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-23 00:50:34 -0700

    Define a vector in abstract syntax.
    Not sure if correct.

--- cs381/hw1/MiniLogoSyntax.hs
+++ cs381/hw1/MiniLogoSyntax.hs
@@ -29,6 +29,11 @@ data Vals = ValNum Int
 --                             pen down;
 --                             moveto (x2, y2)
 
+vector = Def "vector" (ParsNames "x1" (ParsNames "y1" (ParsNames "x2" (ParsName "y2"))))
+                      (Mult (Pen Up)
+                      (Mult (MoveTo (PosName "x1") (PosName "y1"))
+                      (Mult (Pen Down)
+                            (MoveTo (PosName "x2") (PosName "y2")))))
 
 -- vim: expandtab
 
