4256b6f359442b9ec5b8332d5ac5d063b2006cab
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-23 02:42:29 -0700

    Finish part 2b.

--- cs381/hw1/DesAS.hs
+++ cs381/hw1/DesAS.hs
@@ -16,6 +16,14 @@ data Exp = Num Int
 
 altExp = Apply Multiply [Apply Negate [Apply Add [Num 3, Num 4]], Num 7]
 
+-- 2b) The alternate expression has no contraint on how many operands an
+-- operator can be applied to, which makes it more concise when there are
+-- more than two operands. On the flipside, because there is no constraint, it
+-- is possible to apply an operator such as Add to only a single or no operand,
+-- which makes no sense. The flexibility of the alternative representation
+-- allows the user to more freely express expressions but also leaves it up to
+-- the user to do so properly.
+
 
 -- vim: expandtab
 
