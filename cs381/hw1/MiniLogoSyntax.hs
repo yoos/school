19e646927fce9bb61266b2616bf9949d5ef2b93d
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-22 22:28:58 -0700

    Do things right.
    I think?

--- cs381/hw1/MiniLogoSyntax.hs
+++ cs381/hw1/MiniLogoSyntax.hs
@@ -2,10 +2,25 @@ module MiniLogoSyntax where
 
 data Cmd = Pen Mode
          | MoveTo Pos Pos
-         | Def Name Pars Cmd
-         | Call Name Vals
-         | Cmd Cmd
+         | Def String Pars Cmd
+         | Call String Vals
+         | Mult Cmd Cmd
+         deriving Show
 
+data Mode = Up | Down
+          deriving Show
+
+data Pos = Num Int
+         | Name String
+         deriving Show
+
+data Pars = Name String
+          | Name String Pars
+          deriving Show
+
+data Vals = Num Int
+          | Num Int Vals
+          deriving Show
 
 
 -- vim: expandtab
