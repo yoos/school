b16c38ba274f8e185bf968456343b4ed622714d0
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-27 17:35:21 -0700

    Use tabs for indent.

--- engr421/rqt_cb_gui/src/rqt_cb_gui/cb_gui.cpp
+++ engr421/rqt_cb_gui/src/rqt_cb_gui/cb_gui.cpp
@@ -5,54 +5,54 @@
 namespace rqt_cb_gui {
 
 CBGUI::CBGUI()
-  : rqt_gui_cpp::Plugin()
-  , widget_(0)
+	: rqt_gui_cpp::Plugin()
+	, widget_(0)
 {
-  // Constructor is called first before initPlugin function, needless to say.
+	// Constructor is called first before initPlugin function, needless to say.
 
-  // give QObjects reasonable names
-  setObjectName("CBGUI");
+	// give QObjects reasonable names
+	setObjectName("CBGUI");
 }
 
 void CBGUI::initPlugin(qt_gui_cpp::PluginContext& context)
 {
-  // access standalone command line arguments
-  QStringList argv = context.argv();
-  // create QWidget
-  widget_ = new QWidget();
-  // extend the widget with all attributes and children from UI file
-  ui_.setupUi(widget_);
-  // add widget to the user interface
-  context.addWidget(widget_);
+	// access standalone command line arguments
+	QStringList argv = context.argv();
+	// create QWidget
+	widget_ = new QWidget();
+	// extend the widget with all attributes and children from UI file
+	ui_.setupUi(widget_);
+	// add widget to the user interface
+	context.addWidget(widget_);
 }
 
 void CBGUI::shutdownPlugin()
 {
-  // TODO unregister all publishers here
+	// TODO unregister all publishers here
 }
 
 void CBGUI::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& 
 instance_settings) const
 {
-  // TODO save intrinsic configuration, usually using:
-  // instance_settings.setValue(k, v)
+	// TODO save intrinsic configuration, usually using:
+	// instance_settings.setValue(k, v)
 }
 
 void CBGUI::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& 
 instance_settings)
 {
-  // TODO restore intrinsic configuration, usually using:
-  // v = instance_settings.value(k)
+	// TODO restore intrinsic configuration, usually using:
+	// v = instance_settings.value(k)
 }
 
 /*bool hasConfiguration() const
 {
-  return true;
+	return true;
 }
 
 void triggerConfiguration()
 {
-  // Usually used to open a dialog to offer the user a set of configuration
+	// Usually used to open a dialog to offer the user a set of configuration
 }*/
 
 } // namespace
