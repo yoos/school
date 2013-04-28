88dd348e4c242d66e70becea1322cf8c8191c752
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-27 17:30:53 -0700

    Use tabs for indentation.

--- engr421/rqt_cb_gui/src/rqt_cb_gui/cb_gui.h
+++ engr421/rqt_cb_gui/src/rqt_cb_gui/cb_gui.h
@@ -8,24 +8,22 @@
 namespace rqt_cb_gui {
 
 class CBGUI
-  : public rqt_gui_cpp::Plugin
+	: public rqt_gui_cpp::Plugin
 {
-  Q_OBJECT
+	Q_OBJECT
 public:
-  CBGUI();
-  virtual void initPlugin(qt_gui_cpp::PluginContext& context);
-  virtual void shutdownPlugin();
-  virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& 
-instance_settings) const;
-  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& 
-instance_settings);
+	CBGUI();
+	virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+	virtual void shutdownPlugin();
+	virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
+	virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
 
-  // Comment in to signal that the plugin has a way to configure it
-  //bool hasConfiguration() const;
-  //void triggerConfiguration();
+	// Comment in to signal that the plugin has a way to configure it
+	//bool hasConfiguration() const;
+	//void triggerConfiguration();
 private:
-  Ui::CBGUIWidget ui_;
-  QWidget* widget_;
+	Ui::CBGUIWidget ui_;
+	QWidget* widget_;
 };
 } // namespace
 #endif // rqt_cb_gui__CBGUI_H
