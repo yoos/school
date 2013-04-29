2a91ee9ddd74946be194a340fb2b93fcff7199f0
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-28 21:58:49 -0700

    Make publisher callback private.

--- engr421/rqt_cb_gui/src/rqt_cb_gui/cb_gui.h
+++ engr421/rqt_cb_gui/src/rqt_cb_gui/cb_gui.h
@@ -21,7 +21,6 @@ public:
 	virtual void shutdownPlugin();
 	virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
 	virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
-	virtual void pub_cb(void);   // Publisher callback.
 
 	// Comment in to signal that the plugin has a way to configure it
 	//bool hasConfiguration() const;
@@ -33,6 +32,9 @@ private:
 	ros::NodeHandle nh_;   // Private nodehandle.
 	rqt_cb_gui::cb_params cb_params_msg;
 	ros::Publisher cb_gui_pub;
+	ros::Timer pub_timer;
+
+	void pub_cb(const ros::TimerEvent& event);   // Publisher callback.
 };
 } // namespace
 #endif // rqt_cb_gui__CBGUI_H
