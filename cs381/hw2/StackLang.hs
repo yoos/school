33b8d0dda75dcb15a33e6f6280319a45aba6d344
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-29 21:37:32 -0700

    Shaky start. Lots of things are wrong here.

--- cs381/hw2/StackLang.hs
+++ cs381/hw2/StackLang.hs
@@ -4,6 +4,7 @@ module StackLang where
 
 type Prog = [Cmd]
 
+-- Abstract syntax
 data Cmd = LD Int
          | ADD
          | MULT
@@ -12,6 +13,18 @@ data Cmd = LD Int
 
 type Stack = [Int]
 
+type D = Stack -> Stack
+
+
+sem :: Prog -> D   -- Semantic domain
+sem [] i = i
+sem (o:os) i = sem os (semCmd o i)
+
+semCmd :: Cmd -> D
+semCmd LD     = \_ -> i
+semCmd ADD    = \i j -> i + j
+semCmd MULT   = \i j -> i * j
+semCmd DUP    = \i -> i * 2
 
 
 -- vim: expandtab
