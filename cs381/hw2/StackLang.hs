d1130e18062431fa19064f479f7a3fce0aefa590
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-29 23:12:57 -0700

    Finish exercise 1.

--- cs381/hw2/StackLang.hs
+++ cs381/hw2/StackLang.hs
@@ -13,18 +13,43 @@ data Cmd = LD Int
 
 type Stack = [Int]
 
-type D = Stack -> Stack
+-- Semantic domain defined as Maybe to account for errors.
+type D = Maybe Stack -> Maybe Stack
 
-
-sem :: Prog -> D   -- Semantic domain
+-- Semantic function
+sem :: Prog -> D
 sem [] i = i
 sem (o:os) i = sem os (semCmd o i)
 
 semCmd :: Cmd -> D
-semCmd LD     = \_ -> i
-semCmd ADD    = \i j -> i + j
-semCmd MULT   = \i j -> i * j
-semCmd DUP    = \i -> i * 2
+semCmd (LD i) is =   -- Load i onto stack.
+    case is of
+        Just is -> Just (is ++ [i])
+        _       -> Nothing
+semCmd ADD is =   -- Remove two topmost integers from stack and put their sum onto stack.
+    case is of
+        Just is -> if length is < 2 then Nothing
+                                    else Just (init(init is) ++ [last is + last(init is)])
+        _       -> Nothing
+semCmd MULT is =   -- Remove two topmost integers from stack and put their product onto stack.
+    case is of
+        Just is -> if length is < 2 then Nothing
+                                    else Just (init(init is) ++ [last is * last(init is)])
+        _       -> Nothing
+semCmd DUP is =   -- Place second copy of topmost integer onto stack.
+    case is of
+        Just is -> if length is < 1 then Nothing
+                                    else Just (is ++ [last is])
+        _       -> Nothing
+
+-- Some programs
+p = [LD 3, DUP, ADD, DUP, MULT]
+p' = [LD 3, ADD]
+p'' = []
+
+-- A stack
+s :: Maybe [Int]   -- Without this, GHC assumes s is of type Maybe [Integer] instead of Maybe [Int]. Why?
+s = Just [1, 2, 3]
 
 
 -- vim: expandtab
