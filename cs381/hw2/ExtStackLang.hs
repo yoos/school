9e4cc03a714d48af50139cc9a8a60cd1e7a3a9a8
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-30 01:02:39 -0700

    Fix semantic function to use State, not Stack.

--- cs381/hw2/ExtStackLang.hs
+++ cs381/hw2/ExtStackLang.hs
@@ -2,50 +2,64 @@
 
 module ExtStackLang where
 
-type Prog = [ExtCmd]
+type ExtProg = [ExtCmd]
 type Stack = [Int]
 
 -- Abstract syntax
-data ExtCmd = LD Int
-            | ADD
-            | MULT
-            | DUP
+data ExtCmd = EXTLD Int
+            | EXTADD
+            | EXTMULT
+            | EXTDUP
             | DEF String
             | CALL String
             deriving Show
 
 -- Semantic domain defined as Maybe to account for errors.
-type D = Maybe Stack -> Maybe Stack
+type D = Maybe State -> Maybe State
 
-type Macros = [(String, Prog)]   -- List of macro names and the programs they represent.
+type Macros = [(String, ExtProg)]   -- List of macro names and the programs they represent.
 type State = (Macros, Stack)   -- Language state.
 
 -- Semantic function
-sem2 :: Prog -> D
+sem2 :: ExtProg -> D
 sem2 [] i = i
-sem2 (o:os) i = sem os (semCmd2 o i)
+sem2 (o:os) i = sem2 os (semCmd2 o i)
 
 semCmd2 :: ExtCmd -> D
-semCmd2 (LD i) is =   -- Load i onto stack.
-    case is of
-        Just is -> Just (is ++ [i])
+semCmd2 (EXTLD i) (Just (ms,is)) =   -- Load i onto stack.
+    case Just is of
+        Just is -> Just (ms, is ++ [i])
         _       -> Nothing
-semCmd2 ADD is =   -- Remove two topmost integers from stack and put their sum onto stack.
-    case is of
+semCmd2 EXTADD (Just (ms,is)) =   -- Remove two topmost integers from stack and put their sum onto stack.
+    case Just is of
         Just is -> if length is < 2 then Nothing
-                                    else Just (init(init is) ++ [last is + last(init is)])
+                                    else Just (ms, init(init is) ++ [last is + last(init is)])
         _       -> Nothing
-semCmd2 MULT is =   -- Remove two topmost integers from stack and put their product onto stack.
-    case is of
+semCmd2 EXTMULT (Just (ms,is)) =   -- Remove two topmost integers from stack and put their product onto stack.
+    case Just is of
         Just is -> if length is < 2 then Nothing
-                                    else Just (init(init is) ++ [last is * last(init is)])
+                                    else Just (ms, init(init is) ++ [last is * last(init is)])
         _       -> Nothing
-semCmd2 DUP is =   -- Place second copy of topmost integer onto stack.
-    case is of
+semCmd2 EXTDUP (Just (ms,is)) =   -- Place second copy of topmost integer onto stack.
+    case Just is of
         Just is -> if length is < 1 then Nothing
-                                    else Just (is ++ [last is])
+                                    else Just (ms, is ++ [last is])
         _       -> Nothing
 
+{-
+ - In the above semantic function definition, I wanted to do something like
+ - this:
+ -
+ -     semCmd2 (EXTLD i) (ms,is) =
+ -         case is of
+ -             Just is -> Just (ms, is ++ [i])
+ -             _       -> Nothing
+ -
+ - but GHC complained that it "Couldn't match expected type `Maybe State' with
+ - actual type `(t0, t1)'"
+ -
+ - What I've done instead seems like an unnecessary hack.
+ -}
 
 
 
