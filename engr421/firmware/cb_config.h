a468c2e8b485351eb1a4acd9ae88dfba51ad2128
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-02 00:10:51 -0700

    Define indices for arrays.

--- engr421/firmware/cb_config.h
+++ engr421/firmware/cb_config.h
@@ -4,6 +4,7 @@
 /* Timesteps in seconds */
 #define CONTROL_LOOP_DT 0.001
 #define DEATH_RAY_DT    0.00125
+#define HOPPER_DT       0.002
 #define LINEAR_RAIL_DT  0.002
 
 /* Controller parameters */
@@ -14,6 +15,9 @@
 #define DEATH_RAY_I_CAP 15   // I term cap. The right value here can minimize positive overshoot.
 #define DEATH_RAY_STARTUP_COUNTER_MAX 200
 #define DEATH_RAY_STARTUP_DC 0.60
+
+#define HOPPER_DC_MIN 0.0
+#define HOPPER_DC_MAX 1.0
 
 #define LINEAR_RAIL_POS_KP (1)
 #define LINEAR_RAIL_POS_KI (0)
@@ -35,10 +39,28 @@
 #define COMM_INPUT_MAX 250   // Highest valid command byte value (lowest is 0).
 
 /* Indices */
+#define I_ADC_0 0
+#define I_ADC_1 1
+#define I_ADC_2 2
+#define I_ADC_3 3
 #define I_ICU_DEATH_RAY_0   2
 #define I_ICU_DEATH_RAY_1   3
 #define I_ICU_LINEAR_RAIL_0 4
 #define I_ICU_LINEAR_RAIL_1 5
+#define I_DIGITAL_LINEAR_RAIL_0 0
+#define I_DIGITAL_LINEAR_RAIL_1 1
+#define I_DIGITAL_2 2
+#define I_DIGITAL_3 3
+#define I_DIGITAL_4 4
+#define I_DIGITAL_5 5
+#define I_DIGITAL_6 6
+#define I_DIGITAL_7 7
+#define I_PWM_DEATH_RAY_0 0
+#define I_PWM_DEATH_RAY_1 1
+#define I_PWM_LINEAR_RAIL_0 2
+#define I_PWM_LINEAR_RAIL_1 3
+#define I_PWM_HOPPER_0 4
+#define I_PWM_HOPPER_1 5
 
 /* Constants */
 #define M_PI 3.14159
@@ -51,7 +73,7 @@
 
 #define REVS_PER_LENGTH 8.3   // Number of revolutions encoder pulley spins as it moves from one end of the slide to the other.
 
-#define ESC_MIN_DC 0.475   // Minimum duty cycle for Mikrokopter ESCs.
+#define ESC_MIN_DC 0.472   // Minimum duty cycle for Mikrokopter ESCs.
 #define ESC_MAX_DC 0.83    // Maximum duty cycle for Mikrokopter ESCs.
 
 #endif /* CB_CONFIG_H */
