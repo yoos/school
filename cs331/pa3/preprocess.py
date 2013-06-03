6717fa8631b0f550deb73b76d7bd9591383f0471
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-03 13:20:50 -0700

    Add some debug prints.

--- cs331/pa3/preprocess.py
+++ cs331/pa3/preprocess.py
@@ -11,7 +11,9 @@ raw_train = [word for word in line]
 raw_test  = [word for line in list(csv.reader(open('files/raw.test.txt', 'rU'),       delimiter=' ', quotechar='"')) for word in line]
 
 # Remove stopwords from raw_vocab
+print len(raw_vocab), "in raw vocabulary."
 raw_vocab = [word for word in raw_vocab if word not in stoplist]   # 47026 words
+print len(raw_vocab), "in vocabulary after removing stopwords."
 
 # Convert list to dict.
 raw_vocab_dict = dict([(word, 0) for word in raw_vocab])
@@ -24,7 +26,7 @@ for word in raw_test:
     try: raw_vocab_dict[word] += 1
     except KeyError: pass
 
-# Remove uncommon (i.e., used <= 3 times) words. len(raw_vocab_dict) should be 17088 after this.
+# Remove uncommon (i.e., used <= 3 times) words.
 for word in raw_vocab:
     if raw_vocab_dict[word] <= 3:
         del raw_vocab_dict[word]
@@ -35,11 +37,16 @@ clean_vocab = [word for word in raw_vocab if word in raw_vocab_dict]
 # Featurize training data.
 num_clean_vocab   = len(clean_vocab)
 num_train_reviews = len(raw_train_unflat)
+
+print num_clean_vocab, "in vocabulary after removing uncommon words."
+
+# Allocate meomory.
 feature_list = [[0] * num_clean_vocab] * num_train_reviews
 
 
 out_file = open('training.txt', 'wb')
 writer = csv.writer(out_file, delimiter=',', quotechar='"')
+writer.writerow(clean_vocab)
 writer.writerows(feature_list)
 out_file.close()
 
