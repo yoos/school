2e88f41a289cf43074f89342a8be933d587bf2b0
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-03 12:44:24 -0700

    Read in text better.

--- cs331/pa3/preprocess.py
+++ cs331/pa3/preprocess.py
@@ -4,8 +4,8 @@ import csv
 import string
 
 # Read in files. Note that csv.reader will read in each line as a list of words.
-raw_vocab = [word[0] for word in list(csv.reader(open('files/raw.vocabulary.txt', 'rU'), delimiter=',', quotechar='"')) if len(word) > 0]   # 47525 words
-stoplist  = [word[0] for word in list(csv.reader(open('files/stoplist.txt', 'rU'),       delimiter=',', quotechar='"')) if len(word) > 0]
+raw_vocab = [word for line in list(csv.reader(open('files/raw.vocabulary.txt', 'rU'), delimiter=',', quotechar='"')) for word in line]   # 47525 words
+stoplist  = [word for line in list(csv.reader(open('files/stoplist.txt', 'rU'),       delimiter=',', quotechar='"')) for word in line]
 
 # Remove stopwords from raw_vocab
 raw_vocab = [word for word in raw_vocab if word not in stoplist]   # 47026 words
