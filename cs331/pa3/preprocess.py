43a4cb01b5ef349d7e1c9fd17001c7d3dc33ad1d
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-03 12:37:45 -0700

    Convert list to dict.

--- cs331/pa3/preprocess.py
+++ cs331/pa3/preprocess.py
@@ -3,8 +3,15 @@
 import csv
 import string
 
-raw_vocab = list(csv.reader(open('files/raw.vocabulary.txt', 'rU'), delimiter=',', quotechar='"'))
+# Read in files. Note that csv.reader will read in each line as a list of words.
+raw_vocab = [word[0] for word in list(csv.reader(open('files/raw.vocabulary.txt', 'rU'), delimiter=',', quotechar='"')) if len(word) > 0]   # 47525 words
+stoplist  = [word[0] for word in list(csv.reader(open('files/stoplist.txt', 'rU'),       delimiter=',', quotechar='"')) if len(word) > 0]
 
+# Remove stopwords from raw_vocab
+raw_vocab = [word for word in raw_vocab if word not in stoplist]   # 47026 words
+
+# Convert list to dict.
+raw_vocab_dict = dict([(word, 0) for word in raw_vocab])
 
 
 
