fde56df1688f71a1656b2452c00733427c496e94
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-04 01:30:53 -0700

    Add files for HW5.

---
+++ cs381/hw5/hw5.pl
@@ -0,0 +1,27 @@
+
+/* Exercise 1 */
+
+when(275,10).
+when(261,12).
+when(381,11).
+when(398,12).
+when(399,12).
+
+where(275,owen102).
+where(261,dear118).
+where(381,cov216).
+where(398,dear118).
+where(399,cov216).
+
+enroll(mary,275).
+enroll(john,275).
+enroll(mary,261).
+enroll(john,381).
+enroll(jim,399).
+
+
+
+
+/* Exercise 2 */
+
+
