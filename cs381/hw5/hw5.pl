0c731468e79b5b15a3048d1b4495489061e0b97b
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-04 05:33:07 -0700

    Finish Exercise 2.

--- cs381/hw5/hw5.pl
+++ cs381/hw5/hw5.pl
@@ -34,4 +34,23 @@ meet(X, Y) :- schedule(X, P, T), schedule(Y, P, S), X \= Y, S is T+1.
 
 /* Exercise 2 */
 
+/* a */
+rdup([], []).
+rdup([X,X|Y], M) :- rdup([X|Y], M), !.
+rdup([X|Y], [X|Z]) :-  rdup(Y, Z).
+
+/* b */
+flat([], []).
+flat([[]|Y], L) :- flat(Y, L).
+flat([[X|Y]|Z], L) :- flat([X,Y|Z], L), !.
+flat([X|Y], [X|Z]) :- flat(Y, Z).
+
+/* c */
+decrement([], []).
+decrement([A|B], [C|D]) :- decrement(B, D), C is A-1.
+
+project([], _, []).
+project(_, [], []).
+project([X|Y], [A|B], [A|Z]) :- decrement(Y, N), project(N, B, Z), X is 1.
+project([X|Y], [_|B], L)     :- decrement([X|Y], N), project(N, B, L), not(X is 1).
 
