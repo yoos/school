2aa0257fc02975d48147048f3876b5f021792b4c
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-04 00:05:47 -0700

    Classifier coming along.

--- cs331/pa3/naivebayes.py
+++ cs331/pa3/naivebayes.py
@@ -3,6 +3,7 @@
 import sys
 import csv
 import multiprocessing
+from math import log
 
 ### CONSTANTS ###
 NUM_PROC = 10
@@ -29,22 +30,22 @@ test_data  = test_data[1:]
 
 # Count some stuff.
 num_vocab = len(vocab)
-num_train_data = len(train_data) - 1
-num_test_data  = len(test_data)  - 1
+num_train_data = len(train_data)
+num_test_data  = len(test_data)
 
 # Count number of positive/negative reviews.
-train_pos = 0
-test_pos = 0
+num_train_pos = 0
+num_test_pos  = 0
 
 for i in range(1, num_train_data):
     if train_label[i] == "pos":
-        train_pos += 1
+        num_train_pos += 1
 for i in range(1, num_test_data):
     if test_label[i] == "pos":
-        test_pos += 1
+        num_test_pos += 1
 
-train_neg = num_train_data - train_pos   # For convenience.
-test_neg  = num_test_data  - test_pos
+num_train_neg = num_train_data - num_train_pos   # For convenience.
+num_test_neg  = num_test_data  - num_test_pos
 
 
 print "Training Bayes classifier."
@@ -57,7 +58,10 @@ print "Training Bayes classifier."
 # each value of the parameter is equally likely in the absence of data (i.e.,
 # we're assuming prior knowledge). As data is collected, the prior term is
 # dominated.
+#
+# TODO: I only need half of these entries, but first check that my current numbers make sense.
 parameter_list = [[1.0/num_vocab] * 4 + [i] for i in range(num_vocab)]
+prob_review_pos = float(num_train_pos)/num_train_data
 
 # Synchronized counter used to track progress.
 progress = multiprocessing.Value('i', 1)
@@ -68,17 +72,25 @@ def train(parameter):
     vocab_num = parameter[-1]
 
     # Start obfuscation so multiprocessing will work.
+    in_review_count = 0
     for i in range(num_train_data):
-        offset = 2*int(train_data[i][vocab_num])   # If the word is in the review (i.e., feature = 1), we need to update elements 2 and 3.
+        if train_data[i][vocab_num] == '1':   # If the word is in the review (i.e., feature = 1)..
+            in_review_count += 1
+            num_elements = in_review_count
+            offset = 2   # We need to update elements 2 and 3.
+        else:
+            num_elements = (i+1) - in_review_count
+            offset = 0
+
         r = 1   # Update the appropriate elements.
         if train_label[i] == "pos": r = 0
 
-        parameter[offset+0] = (i*parameter[offset+0]+r)    /(i+1)
-        parameter[offset+1] = (i*parameter[offset+1]+(1-r))/(i+1)
+        parameter[offset+0] = ((num_elements-1) * parameter[offset+0]+r)     / num_elements
+        parameter[offset+1] = ((num_elements-1) * parameter[offset+1]+(1-r)) / num_elements
 
     parameter.pop()   # Remove the index element.
 
-    print "Progress:", str(progress.value)+"/"+str(num_vocab), parameter
+    print "Progress:", str(progress.value)+"/"+str(num_vocab), parameter, in_review_count
     with lock:
         progress.value += 1
     return parameter
@@ -87,4 +99,54 @@ p = multiprocessing.Pool(NUM_PROC)
 parameter_list = p.map(train, parameter_list)
 p.close()
 p.join()
+
+
+print "Classifying testing data."
+
+# Allocate memory for testing data classifications.
+review_list = [["unknown", i] for i in range(num_test_data)]
+
+# Reset counter.
+progress.value = 1
+
+# Classify.
+def classify(review):
+    review_idx = review[-1]
+
+    # Use logs so we're not comparing zeros.
+    prob_pos = log(prob_review_pos)
+    prob_neg = log(1-prob_review_pos)
+
+    # Consider each word.
+    for i in range(num_vocab):
+        if test_data[review_idx][i] == '1':   # If the word is in the review (i.e., feature = 1)..
+            prob_pos += log(parameter_list[i][3]+1e-100)
+            prob_neg += log(parameter_list[i][2]+1e-100)
+
+    review.pop()   # Remove the index element.
+
+    if prob_pos > prob_neg:
+        review[0] = "pos"
+    else:
+        review[0] = "neg"
+
+    print "Progress:", str(progress.value)+"/"+str(num_test_data), prob_pos, prob_neg, review[0], test_label[review_idx]
+    with lock:
+        progress.value += 1
+    return review
+
+p = multiprocessing.Pool(NUM_PROC)
+review_list = p.map(classify, review_list)
+p.close()
+p.join()
+
+
+# Calculate accuracy.
+num_accurate = 0.0
+for i in range(num_test_data):
+    if review_list[i][0] == test_label[i]:
+        num_accurate += 1.0
+accuracy = num_accurate/num_test_data
+
+print "Accuracy:", accuracy
 
