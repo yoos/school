fdb49f96b2e4e82a12707ba7db5c0182f3e5e1ec
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-04 02:54:22 -0700

    Finish Exercise 1.

--- cs381/hw5/hw5.pl
+++ cs381/hw5/hw5.pl
@@ -19,7 +19,17 @@ enroll(mary,261).
 enroll(john,381).
 enroll(jim,399).
 
+/* a */
+schedule(S, P, T) :- enroll(S, C), when(C, T), where(C, P).
 
+/* b */
+usage(P, T) :- when(C, T), where(C, P).
+
+/* c */
+conflict(X, Y) :- where(X, P), where(Y, P), when(X, T), when(Y, T), X \= Y.
+
+/* d */
+meet(X, Y) :- schedule(X, P, T), schedule(Y, P, S), X \= Y, S is T+1.
 
 
 /* Exercise 2 */
