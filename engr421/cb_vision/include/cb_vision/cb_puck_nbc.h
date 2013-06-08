0d5d25bc813e32285788bd161479bdc3b0b370b5
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-08 13:38:32 -0700

    Add a Naive Bayes classifier for finding pucks.

---
+++ engr421/cb_vision/include/cb_vision/cb_puck_nbc.h
@@ -0,0 +1,46 @@
+#ifndef CB_PUCK_NBC_H
+#define CB_PUCK_NBC_H
+
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <cb_vision/cb_math.h>
+
+
+using namespace cv;
+
+typedef struct {
+	float encircle_size;   // Size of minimum enclosing circle.
+	float puck_encircle_ratio;   // Ratio of contour to enclosing circle.
+	float dist_last_closest_puck;   // Distance to the last closest puck.
+} puck_features;
+
+typedef struct {
+	float encircle_size;
+	float puck_encircle_ratio;
+	float dist_last_closest_puck;
+} puck_probabilities;
+
+typedef struct
+{
+	Point              loc;   // Location of puck so we can use it later.
+	puck_probabilities prob;
+	float              puckiness;   // Probability from 0.0 to 1.0 that this is a puck.
+} puck_parameter;
+
+
+class CBNaiveBayesPuckifier
+{
+	puck_features ipf;   // Ideal puck features.
+	vector<puck_parameter> pp;   // Puck parameters.
+	puck_parameter puckiest_pucks[2];
+
+public:
+	CBNaiveBayesPuckifier(void);
+
+	void set_ideal_puck_features(puck_features pf);
+	void add_potential_puck(Point loc, puck_features pf);
+	void get_puckiest_pucks(Point pucks[2]);
+};
+
+#endif // CB_PUCK_NBC_H
+
---
+++ engr421/cb_vision/src/cb_puck_nbc.cpp
@@ -0,0 +1,45 @@
+#include <cb_vision/cb_puck_nbc.h>
+
+CBNaiveBayesPuckifier::CBNaiveBayesPuckifier(void)
+{
+	// Do nothing
+}
+
+void CBNaiveBayesPuckifier::set_ideal_puck_features(puck_features pf)
+{
+	ipf.encircle_size = pf.encircle_size;
+	ipf.puck_encircle_ratio = pf.puck_encircle_ratio;
+	ipf.dist_last_closest_puck = pf.dist_last_closest_puck;
+}
+
+void CBNaiveBayesPuckifier::add_potential_puck(Point loc, puck_features pf)
+{
+	puck_parameter new_pp;
+	new_pp.loc = loc;
+
+	new_pp.prob.encircle_size  = 1.0 - MIN(1.0, ABS(ipf.encircle_size - pf.encircle_size)/5);
+	new_pp.prob.puck_encircle_ratio = 1.0 - MIN(1.0, ABS(ipf.puck_encircle_ratio - pf.puck_encircle_ratio));
+	new_pp.prob.dist_last_closest_puck = 1.0 - MIN(1.0, 0.0);   // TODO
+
+	new_pp.puckiness = new_pp.prob.encircle_size * new_pp.prob.puck_encircle_ratio * new_pp.prob.dist_last_closest_puck;
+
+	// Keep track of all the potential pucks, just in case. I might remove this
+	// later.
+	pp.push_back(new_pp);
+
+	// Keep track of the two puckiest pucks.
+	if (new_pp.puckiness > puckiest_pucks[0].puckiness) {
+		puckiest_pucks[1] = puckiest_pucks[0];
+		puckiest_pucks[0] = new_pp;
+	}
+	else if (new_pp.puckiness > puckiest_pucks[1].puckiness) {
+		puckiest_pucks[1] = new_pp;
+	}
+}
+
+void CBNaiveBayesPuckifier::get_puckiest_pucks(Point pucks[2])
+{
+	pucks[0] = puckiest_pucks[0].loc;
+	pucks[1] = puckiest_pucks[1].loc;
+}
+
