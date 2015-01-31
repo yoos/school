c2b530b1826a0bf3d02127f07a86254c89f4f24c
Soo-Hyun Yoo <yoos117@gmail.com>
2015-01-30 23:47:41 -0800

    Progress.

--- cs480/m2/lexer.lisp
+++ cs480/m2/lexer.lisp
@@ -10,65 +10,124 @@
                                   :fill-pointer 0
                                   :adjustable t))
 (defparameter *state* :find-token)    ; FSA state
+(defparameter *type* :unknown-t)      ; Current token type
+
+(defun letter? (c)
+  (or (and (string>= c "A") (string<= c "Z"))
+      (and (string>= c "a") (string<= c "z"))))
+
+(defun digit? (c)
+  (and (string>= c "0") (string<= c "9")))
+
+(defun buildtoken (istream token)
+  (let (c '(read-char istream nil))
+    (vector-push-extend c token)
+    c))
+
+;(defun update-state (fsa-state fsa-type)
+;  (defvar *state* fsa-state)
+;  (defvar *type* fsa-type))
+;
+(defun store-token (token-type token-string)
+  (format T "[TOKEN] <~A ~A>~%" token-type token-string)
+  (defparameter *state* :find-token)
+  (defparameter *type* :unknown-t)
+  (defparameter *token* (make-array 0
+                                    :element-type 'character
+                                    :fill-pointer 0
+                                    :adjustable t)))
 
 
-(with-open-file (stream "inputs/1.txt")
-  (cond
-    ;; Find new token
-    ((eq *state* :find-token)
-     (format T "[State FSA] Find token")
+(with-open-file (istream "inputs/2.txt")
+  ;; Read in one character
+  (do
+    ((c (read-char istream NIL)     ; Start with first char read
+        (read-char istream NIL)))   ; Read another char each step
+    ((null c))                      ; End when c is null
+    (vector-push-extend c *token*)   ; Append char to token
 
-     ;; Read in one character
-     (do
-       ((c (read-char stream nil)
-           (read-char stream nil)))
-       ((null c))
-       (vector-push-extend c *token*)   ; Append char to token
-
-       ;; Match
+    (cond
+      ;; Letters
+      ((letter? c)
+       ;; More letters, non-numerals, etc?
+       (do
+         ((c (read-char istream NIL)
+             (read-char istream NIL)))
+         ((not (letter? c))   ; We should allow numbers, too
+          (unread-char c istream))
+         (vector-push-extend c *token*))
        (cond
-         ;; A-Z, a-z
-         ((or (and (string>= c "A") (string<= c "Z"))
-              (and (string>= c "a") (string<= c "z")))
-          (format T "[Token FSA] Character~%"))
+         ;; Check if primitive type
+         ((string= *token* "bool")
+          (defparameter *type* :boolean-tt))
+         ((string= *token* "int")
+          (defparameter *type* :integer-tt))
+         ((string= *token* "real")
+          (defparameter *type* :real-tt))
+         ((string= *token* "string")
+          (defparameter *type* :string-tt))
 
-         ;; 0-9
-         ((and (string>= c "0") (string<= c "9"))
-          (format T "[Token FSA] Number~%"))
-
-         ;; String
-         ((string= c #\")
-          (format T "[Token FSA] String~%"))
-
-         ;; Op
-         ((member c '(#\( #\)
-                      #\+ #\- #\* #\/ #\^ #\%
-                      #\= #\> #\< #\! #\: #\;))
-          (format T "[Token FSA] Op: ~A~%" c))
-
-
-         ;; Whitespace
-         ((or (string= c " ")
-              (string= c #\tab))
-          (format T "[Token FSA] Whitespace~%"))
-
-         ;; Newline
-         ((string= c #\linefeed)
-          (format T "[Token FSA] Newline~%"))
+         ;; Check if statement
+         ((string= *token* "stdout")
+          (defparameter *type* :stdout-st))
+         ((string= *token* "if")
+          (defparameter *type* :if-st))
+         ((string= *token* "while")
+          (defparameter *type* :while-st))
+         ((string= *token* "let")
+          (defparameter *type* :let-st))
 
          ;; Fallback
          (T
-           (format T "[Token FSA] Unknown: ~A~%" c))
+           (defparameter *type* :identifier-tt))
          )
-       ))
 
-    ;; Token found, so store in table
-    ((eq *state* :store-token)
-     (format T "[State FSA] Store token~%"))
+       ;; Store token and reset FSA
+       (store-token *type* *token*)
+       )
 
-    ;; Fallback case
-    (T
-      (format T "[FSA] Unknown state~%"))))
+      ;; 0-9
+      ((digit? c)
+       (do
+         ((c (read-char istream NIL)
+             (read-char istream NIL)))
+         ((not (digit? c))   ; We should allow exponents and at most one decimal point
+          (unread-char c istream))
+         (vector-push-extend c *token*))
+       (cond
+         ((or (string= *token* "e")
+              (string= *token* "."))
+          (defparameter *type* :real-tt))
+         )
+       (store-token *type* *token*)
+       ;(format T "[Token FSA] Number~%")
+       )
 
-(format T *token*)
+      ;; String
+      ((string= c #\")
+       (format T "[Token FSA] String~%"))
+
+      ;; Op
+      ((member c '(#\( #\)
+                   #\+ #\- #\* #\/ #\^ #\%
+                   #\= #\> #\< #\! #\: #\;
+                   ))   ; The semicolon above screws up syntax highlighting.
+       (format T "[Token FSA] Op: ~A~%" c))
+
+      ;; Whitespace
+      ((or (string= c " ")
+           (string= c #\tab))
+       ;(format T "[Token FSA] Whitespace~%")
+       )
+
+      ;; Newline
+      ((string= c #\linefeed)
+       (format T "[Token FSA] Newline~%"))
+
+      ;; Fallback
+      (T
+        (format T "[Token FSA] Unknown: ~A~%" c))
+      ))
+  )
+
 
