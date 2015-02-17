78bd2487af06e529a5dab1706d6742247416b604
Soo-Hyun Yoo <yoos117@gmail.com>
2015-02-17 15:24:55 -0800

    Process command line arguments.

--- cs480/m3/compiler.lisp
+++ cs480/m3/compiler.lisp
@@ -4,5 +4,20 @@
 (load "syntax-parser")
 (load "grammar")
 
-(with-open-file (istream "proftest.in")
-  (syntax-parse (lex istream) *grammar* 0))
+(defun my-command-line ()
+  (or 
+    #+SBCL *posix-argv*  
+    #+LISPWORKS system:*line-arguments-list*
+    #+CMU extensions:*command-line-words*
+    nil))
+
+(defparameter args (cdr (my-command-line)))
+
+(loop for arg in args do
+      (cond ((not (string= (subseq arg 0 1) "-"))
+             (format T "Parsing ~S:~%" arg)
+             (with-open-file (istream arg)
+               (syntax-parse (lex istream) *grammar* 0))
+             (format T "~%~%"))
+            (T NIL))
+      )
