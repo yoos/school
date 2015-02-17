299c4da16f917b107530dfe5cdcb5ccb911a242f
Soo-Hyun Yoo <yoos117@gmail.com>
2015-02-16 23:59:37 -0800

    Start making parser recursive.

--- cs480/m3/syntax-parser.lisp
+++ cs480/m3/syntax-parser.lisp
@@ -1,8 +1,18 @@
+(load "tokens")
 
+(defun syntax-parse (symbol-table grammar depth)
+  (do
+    ((sym (vector-pop symbol-table)
+          (vector-pop symbol-table)))
+    ((= (length symbol-table) 0))
+    ()
+    (let ((token-type (nth 0 sym))
+          (token      (nth 1 sym)))
+      (cond ((string= token "(")
+             (syntax-parse symbol-table grammar depth))
+            ((string= token ")")
+             (return))
+            (T
+              (format T "~v~A~%" depth #\  token))
+      ))))
 
-(defun syntax-parse (symbol-table grammar)
-
-  ;; Print symbol table
-  (format T "[PARSE] token list:~%~S~%" symbol-table)
-
-  (format T "[PARSE] grammar:~S~%" grammar))
