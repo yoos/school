f24b096cf92e17dd748dadc125cf19889c5a6636
Soo-Hyun Yoo <yoos117@gmail.com>
2015-02-16 20:24:03 -0800

    Print grammar from parser.

--- cs480/m3/parser.lisp
+++ cs480/m3/parser.lisp
@@ -1,4 +1,8 @@
-(defun parse (symbol-table)
+
+
+(defun parse (symbol-table grammar)
 
   ;; Print symbol table
-  (format T "[PARSE] token list:~%~S" symbol-table))
+  (format T "[PARSE] token list:~%~S~%" symbol-table)
+
+  (format T "[PARSE] grammar:~S~%" grammar))
