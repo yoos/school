2a609babdb7e058e188f7dca1bb515587bbd8903
Soo-Hyun Yoo <yoos117@gmail.com>
2015-02-17 12:27:04 -0800

    Correctly indent input.

--- cs480/m3/syntax-parser.lisp
+++ cs480/m3/syntax-parser.lisp
@@ -1,18 +1,32 @@
 (load "tokens")
 
+;;; Convenience function so popping from symbol table fails gracefully.
+(defun table-pop (symbol-table)
+  (if (> (fill-pointer symbol-table) 0)
+    (vector-pop symbol-table)))
+
 (defun syntax-parse (symbol-table grammar depth)
   (do
-    ((sym (vector-pop symbol-table)
-          (vector-pop symbol-table)))
-    ((= (length symbol-table) 0))
+    ((sym (table-pop symbol-table)
+          (table-pop symbol-table)))
+    ((null sym))
     ()
     (let ((token-type (nth 0 sym))
           (token      (nth 1 sym)))
-      (cond ((string= token "(")
-             (syntax-parse symbol-table grammar depth))
-            ((string= token ")")
+      ;; Handle closing parenthesis specially
+      (cond ((and (equal token-type :op-t)
+                  (string= token ")"))
+             (format T "~,,v,@A~%" (* 2 (- depth 1)) token)
              (return))
-            (T
-              (format T "~v~A~%" depth #\  token))
-      ))))
+            (T NIL))
 
+      ;; Print to screen at correct indentation
+      ;; See http://stackoverflow.com/questions/20072959/lisp-format-a-character-a-number-of-times
+      (format T "~,,v,@A~%" (* 2 depth) token)
+
+      ;; Recurse on opening parenthesis
+      (cond ((and (equal token-type :op-t)
+                  (string= token "("))
+             (syntax-parse symbol-table grammar (+ 1 depth)))
+            (T NIL))
+      )))
