7c07413660ad02f55c573ce3c45981a427af70b9
Soo-Hyun Yoo <yoos117@gmail.com>
2015-03-01 20:01:12 -0800

    Build tree in syntax parser.

--- cs480/m4/syntax-parser.lisp
+++ cs480/m4/syntax-parser.lisp
@@ -6,25 +6,24 @@
     (vector-pop symbol-table)))
 
 (defun syntax-parse (symbol-table grammar depth)
-  (do
-    ((sym (table-pop symbol-table)
-          (table-pop symbol-table)))
-    ((null sym))
-    ()
-    (let ((token-type (nth 0 sym))
-          (token      (nth 1 sym)))
-      ;; Handle closing parenthesis specially
-      (cond ((equal token-type :rightp-dt)
-             (format T "~,,v,@A~%" (* 2 (- depth 1)) token)
-             (return))
-            (T NIL))
-
-      ;; Print to screen at correct indentation
-      ;; See http://stackoverflow.com/questions/20072959/lisp-format-a-character-a-number-of-times
-      (format T "~,,v,@A~%" (* 2 depth) token)
-
-      ;; Recurse on opening parenthesis
-      (cond ((equal token-type :leftp-dt)
-             (syntax-parse symbol-table grammar (+ 1 depth)))
-            (T NIL))
-      )))
+  (let ((subtree ()))
+    (do
+      ((sym (table-pop symbol-table)
+            (table-pop symbol-table)))
+      ((null sym))
+      ()
+      (let ((token-type (nth 0 sym))
+            (token      (nth 1 sym)))
+        (cond ((equal token-type :rightp-dt)   ;; Handle closing parenthesis specially
+               (format T "~,,v,@A~%" (* 2 (- depth 1)) token)
+               (return))
+              ((equal token-type :leftp-dt)   ;; Recurse on opening parenthesis
+               (format T "~,,v,@A~%" (* 2 depth) token)
+               (setf subtree (cons (syntax-parse symbol-table grammar (+ 1 depth)) subtree)))
+              (T
+                ;; Print to screen at correct indentation
+                ;; See http://stackoverflow.com/questions/20072959/lisp-format-a-character-a-number-of-times
+                (format T "~,,v,@A~%" (* 2 depth) token)
+                (setf subtree (cons sym subtree))
+                ))))
+    (nreverse subtree)))
