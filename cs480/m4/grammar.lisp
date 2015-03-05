c2fb7a7163f44b1ff58d6d9ecf6475bf4d32a114
Soo-Hyun Yoo <yoos117@gmail.com>
2015-03-05 12:45:37 -0800

    Glob statement and constant types together.

--- cs480/m4/grammar.lisp
+++ cs480/m4/grammar.lisp
@@ -1,65 +1,57 @@
 (load "tokens")
 
-(defparameter gN '(:S
-                    :exprs :exprlist
-                    :constants :bools :ints :reals :strings
-                    :ids :idlist
-                    :opers :binops :unops
-                    :prims
-                    :stmts :printstmts :ifstmts :whilestmts :letstmts))   ; Set of non-terminals
-(defparameter gT `(,@constants
-                    ,@delimiters
-                    ,@identifiers
-                    ,@operators
-                    ,@primitives
-                    ,@statements))   ; Set of terminals
-(defparameter gG (list (list :S        (list '(:leftp-dt :rightp-dt)
-                                             '(:leftp-dt :S :rightp-dt)
-                                             '(:S :S)
-                                             '(:exprs)))
-                       (list :exprs    (list '(:opers)
-                                             '(:stmts)))
-                       (list :exprlist (list '(:exprs)
-                                             '(:exprs :exprlist)))
+(defparameter gN (list 'S
+                       'exprs 'exprlist
+                       'constants
+                       'ids 'idlist
+                       'opers 'binops 'unops
+                       'prims
+                       'stmts))   ; Set of non-terminals
+(defparameter gT `(list ,@constants
+                        ,@delimiters
+                        ,@identifiers
+                        ,@operators
+                        ,@primitives
+                        ,@statements))   ; Set of terminals
+(defparameter gG (list (list 'S          (list 'leftp-dt 'rightp-dt))
+                       (list 'S          (list 'leftp-dt 'S 'rightp-dt))
+                       (list 'S          (list 'S 'S))
+                       (list 'S          (list 'exprs))
+                       (list 'exprs      (list 'opers))
+                       (list 'exprs      (list 'stmts))
+                       (list 'exprlist   (list 'exprs))
+                       (list 'exprlist   (list 'exprs 'exprlist))
 
-                       (list :constants (list '(:bools)
-                                              '(:ints)
-                                              '(:reals)
-                                              '(:strings)))
-                       (list :bools     (list '(:boolean-ct)))
-                       (list :ints      (list '(:integer-ct)))
-                       (list :reals     (list '(:real-ct)))
-                       (list :strings   (list '(:string-ct)))
+                       (list 'constants  (list 'boolean-ct))
+                       (list 'constants  (list 'integer-ct))
+                       (list 'constants  (list 'real-ct))
+                       (list 'constants  (list 'string-ct))
 
-                       (list :ids    (list '(:function-it)
-                                           '(:variable-it)))
-                       (list :idlist (list '(:leftp-dt :ids :prims :rightp-dt)
-                                           '(:leftp-dt :ids :prims :rightp-dt :idlist)))
+                       (list 'ids        (list 'function-it))
+                       (list 'ids        (list 'variable-it))
+                       (list 'idlist     (list 'leftp-dt 'ids 'prims 'rightp-dt))
+                       (list 'idlist     (list 'leftp-dt 'ids 'prims 'rightp-dt 'idlist))
 
-                       (list :opers  (list '(:leftp-dt :assign-st :ids :opers :rightp-dt)
-                                           '(:leftp-dt :binops :opers :opers :rightp-dt)
-                                           '(:leftp-dt :unops :opers :rightp-dt)
-                                           '(:constants)
-                                           '(:ids)))
-                       (list :binops (list '(:binop-ot)))
-                       (list :unops  (list '(:unop-ot)))
+                       (list 'opers      (list 'leftp-dt 'assign-st 'ids 'opers 'rightp-dt))
+                       (list 'opers      (list 'leftp-dt 'binops 'opers 'opers 'rightp-dt))
+                       (list 'opers      (list 'leftp-dt 'unops 'opers 'rightp-dt))
+                       (list 'opers      (list 'constants))
+                       (list 'opers      (list 'ids))
+                       (list 'binops     (list 'binop-ot))
+                       (list 'unops      (list 'unop-ot))
 
-                       (list :prims (list '(:boolean-pt)
-                                          '(:integer-pt)
-                                          '(:real-pt)
-                                          '(:string-pt)))
+                       (list 'prims      (list 'boolean-pt))
+                       (list 'prims      (list 'integer-pt))
+                       (list 'prims      (list 'real-pt))
+                       (list 'prims      (list 'string-pt))
 
-                       (list :stmts      (list '(:ifstmts)
-                                               '(:whilestmts)
-                                               '(:letstmts)
-                                               '(:printstmts)))
-                       (list :printstmts (list '(:leftp-dt :stdout-st :opers :rightp-dt)))
-                       (list :ifstmts    (list '(:leftp-dt :if-st :exprs :exprs :exprs :rightp-dt)
-                                               '(:leftp-dt :exprs :exprs)))
-                       (list :whilestmts (list '(:leftp-dt :while-st :exprs :exprlist :rightp-dt)))
-                       (list :letstmts   (list '(:leftp-dt :let-st :leftp-dt :idlist :rightp-dt :rightp-dt)))
+                       (list 'stmts      (list 'leftp-dt 'stdout-st 'opers 'rightp-dt))
+                       (list 'stmts      (list 'leftp-dt 'if-st 'exprs 'exprs 'exprs 'rightp-dt))
+                       (list 'stmts      (list 'leftp-dt 'exprs 'exprs))
+                       (list 'stmts      (list 'leftp-dt 'while-st 'exprs 'exprlist 'rightp-dt))
+                       (list 'stmts      (list 'leftp-dt 'let-st 'leftp-dt 'idlist 'rightp-dt 'rightp-dt))
                        ))   ; Set of productions
-(defparameter gS :S)   ; Start symbol
+(defparameter gS 'S)   ; Start symbol
 
 ;;; Define global grammar
 (defparameter *grammar* (list gN gT gG gS))
