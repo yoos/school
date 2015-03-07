26a5ef74ed32cfa5d8b0cc978a61b8a9eb8944fa
Soo-Hyun Yoo <yoos117@gmail.com>
2015-03-07 01:10:08 -0800

    Print out some steps in compiler.

--- cs480/m4/compiler.lisp
+++ cs480/m4/compiler.lisp
@@ -14,11 +14,21 @@
 
 (defparameter args (cdr (my-command-line)))
 
+
+;;; TODO: I shouldn't declare these globally!
+(defparameter lexer-list ())
+(defparameter syntax-tree ())
+(defparameter semantics-result ())
+
 (loop for arg in args do
       (cond ((not (string= (subseq arg 0 1) "-"))
-             (format T "Parsing ~S:~%" arg)
+             (format T "Parsing ~S:~%~%" arg)
              (with-open-file (istream arg)
-               (format T "~S" (semantics-parse (syntax-parse (lex istream) *grammar* 0) *grammar* 0)))
+               (setf lexer-list       (lex istream)
+                     syntax-tree      (syntax-parse lexer-list *grammar* 0)
+                     semantics-result (semantics-parse syntax-tree *grammar* 0))
+               (format T "~%Syntax tree: ~S~%" syntax-tree)
+               (format T "~%Semantics result: ~S~%" semantics-result))
              (format T "~%~%"))
             (T NIL))
       )
