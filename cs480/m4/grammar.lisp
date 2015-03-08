6efeac5f8c3811dc5eba5ad48888fe836faa4688
Soo-Hyun Yoo <yoos117@gmail.com>
2015-03-07 17:40:19 -0800

    Refactor grammar.

--- cs480/m4/grammar.lisp
+++ cs480/m4/grammar.lisp
@@ -1,10 +1,9 @@
 (load "tokens")
 
-(defparameter gN (list 'S
-                       'exprs 'exprlist
-                       'idlist
-                       'opers
-                       'stmts))   ; Set of non-terminals
+(defparameter gN (list 'S 'expr 'exprs
+                       'ids
+                       'oper
+                       'stmt))   ; Set of non-terminals
 
 (defparameter gT `(list ,@constants
                         ,@delimiters
@@ -13,41 +12,41 @@
                         ,@primitives
                         ,@statements))   ; Set of terminals
 
-(defparameter gG (list (list 'S          (list 'leftp-dt    'rightp-dt))
-                       (list 'S          (list 'leftp-dt 'S 'rightp-dt))
-                       (list 'S          (list 'S 'S))
-                       (list 'S          (list 'exprs))
-                       (list 'exprs      (list 'opers))
-                       (list 'exprs      (list 'stmts))
-                       (list 'exprlist   (list 'exprs))
-                       (list 'exprlist   (list 'exprs 'exprlist))
+(defparameter gG (list (list 'S     (list 'leftp-dt    'rightp-dt))
+                       (list 'S     (list 'leftp-dt 'S 'rightp-dt))
+                       (list 'S     (list 'S 'S))
+                       (list 'S     (list 'expr))
+                       (list 'expr  (list 'oper))
+                       (list 'expr  (list 'stmt))
+                       (list 'exprs (list 'expr))
+                       (list 'exprs (list 'expr 'exprs))
 
-                       (list 'idlist     (list 'leftp-dt 'ids 'prims 'rightp-dt))
-                       (list 'idlist     (list 'leftp-dt 'ids 'prims 'rightp-dt 'idlist))
+                       (list 'ids   (list 'leftp-dt 'id 'prim 'rightp-dt))
+                       (list 'ids   (list 'leftp-dt 'id 'prim 'rightp-dt 'ids))
 
-                       (list 'opers      (list 'leftp-dt 'assign-st 'ids   'opers 'rightp-dt))
-                       (list 'opers      (list 'leftp-dt 'binop-ot  'opers 'opers 'rightp-dt))
-                       (list 'opers      (list 'leftp-dt 'unop-ot   'opers        'rightp-dt))
-                       (list 'opers      (list 'constants))
-                       (list 'opers      (list 'ids))
+                       (list 'oper (list 'leftp-dt 'assign-st 'id   'oper 'rightp-dt))
+                       (list 'oper (list 'leftp-dt 'binop-ot  'oper 'oper 'rightp-dt))
+                       (list 'oper (list 'leftp-dt 'unop-ot   'oper       'rightp-dt))
+                       (list 'oper (list 'const))
+                       (list 'oper (list 'id))
 
-                       (list 'stmts      (list 'leftp-dt 'stdout-st 'opers                         'rightp-dt))
-                       (list 'stmts      (list 'leftp-dt 'if-st     'exprs    'exprs    'exprs     'rightp-dt))
-                       (list 'stmts      (list 'leftp-dt 'if-st     'exprs    'exprs               'rightp-dt))
-                       (list 'stmts      (list 'leftp-dt 'while-st  'exprs    'exprlist            'rightp-dt))
-                       (list 'stmts      (list 'leftp-dt 'let-st    'leftp-dt 'idlist   'rightp-dt 'rightp-dt))
+                       (list 'stmt (list 'leftp-dt 'stdout-st 'oper                       'rightp-dt))
+                       (list 'stmt (list 'leftp-dt 'if-st     'expr     'expr  'expr      'rightp-dt))
+                       (list 'stmt (list 'leftp-dt 'if-st     'expr     'expr             'rightp-dt))
+                       (list 'stmt (list 'leftp-dt 'while-st  'expr     'exprs            'rightp-dt))
+                       (list 'stmt (list 'leftp-dt 'let-st    'leftp-dt 'ids   'rightp-dt 'rightp-dt))
 
                        ;; Singular constant categories
-                       (list 'constants  (list 'boolean-ct))
-                       (list 'constants  (list 'integer-ct))
-                       (list 'constants  (list 'real-ct))
-                       (list 'constants  (list 'string-ct))
-                       (list 'ids        (list 'function-it))
-                       (list 'ids        (list 'variable-it))
-                       (list 'prims      (list 'boolean-pt))
-                       (list 'prims      (list 'integer-pt))
-                       (list 'prims      (list 'real-pt))
-                       (list 'prims      (list 'string-pt))
+                       (list 'const (list 'boolean-ct))
+                       (list 'const (list 'integer-ct))
+                       (list 'const (list 'real-ct))
+                       (list 'const (list 'string-ct))
+                       (list 'id    (list 'function-it))
+                       (list 'id    (list 'variable-it))
+                       (list 'prim  (list 'boolean-pt))
+                       (list 'prim  (list 'integer-pt))
+                       (list 'prim  (list 'real-pt))
+                       (list 'prim  (list 'string-pt))
                        ))   ; Set of productions
 
 (defparameter gS 'S)   ; Start symbol
