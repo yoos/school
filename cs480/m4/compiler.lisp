e713feef44e3726ea40c34db4241c862bcbf1371
Soo-Hyun Yoo <yoos117@gmail.com>
2015-03-08 21:59:41 -0700

    Stop using global vars.

--- cs480/m4/compiler.lisp
+++ cs480/m4/compiler.lisp
@@ -14,21 +14,15 @@
 
 (defparameter args (cdr (my-command-line)))
 
-
-;;; TODO: I shouldn't declare these globally!
-(defparameter lexer-list ())
-(defparameter syntax-tree ())
-(defparameter semantics-result ())
-
 (loop for arg in args do
       (cond ((not (string= (subseq arg 0 1) "-"))
              (format T "Parsing ~S:~%~%" arg)
              (with-open-file (istream arg)
-               (setf lexer-list       (lex istream)
-                     syntax-tree      (syntax-parse lexer-list *grammar* 0)
-                     semantics-result (semantics-parse syntax-tree *grammar* 0))
-               (format T "~%Syntax tree: ~S~%" syntax-tree)
-               (format T "~%Semantics result: ~S~%" semantics-result))
-             (format T "~%~%"))
+               (let* ((lexer-list       (lex istream))
+                      (syntax-tree      (syntax-parse lexer-list *grammar* 0))
+                      (semantics-result (semantics-parse syntax-tree *grammar* 0)))
+                 (format T "~%Syntax tree: ~S~%" syntax-tree)
+                 (format T "~%Semantics result: ~S~%" semantics-result))
+               (format T "~%~%")))
             (T NIL))
       )
