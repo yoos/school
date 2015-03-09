d64172cd77e64a6afba7feb505e29ff151e76d23
Soo-Hyun Yoo <yoos117@gmail.com>
2015-03-09 03:50:12 -0700

    Remove obsolete stuff.
    This whole thing is obsolete..

--- cs480/m4/semantics-parser.lisp
+++ cs480/m4/semantics-parser.lisp
@@ -1,23 +1,8 @@
 (load "tokens")
 
-;;; De-terminalize so we can match against production rules.
-(defun determ (grammar expr-type)
-  (let ((gN (nth 0 grammar))   ; Nonterminals
-        (gG (nth 2 grammar))   ; Productions
-        (matches (mapcar (lambda (R)
-                           (equal (cadr R)
-                                  (list expr-type)))
-                         (nth 2 grammar))))   ; List of rules that match
-    (let ((match-idx (position T matches)))
-      (if (and (not (null match-idx))
-               (not (member expr-type gN)))
-        (car (nth match-idx gG))
-        expr-type))
-    ))
-
 (defun semantics-parse (parse-tree grammar depth)
   (let ((sentence ())
-        (gG (nth 2 grammar)))
+        (gP (nth 2 grammar)))
     (cond
       ;; Base case
       ((typep (car parse-tree) 'token-t)
@@ -40,8 +25,8 @@
         (let ((match-pos (position T (mapcar (lambda (R)
                                                (equal (cadr R)
                                                       sentence))
-                                             gG))))
+                                             gP))))
           (if (null match-pos)
             'error
-            (car (nth match-pos gG))))))
+            (car (nth match-pos gP))))))
     ))
