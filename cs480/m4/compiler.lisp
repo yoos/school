51e3a0b1619357f26b284292d3284fb64d047e81
Soo-Hyun Yoo <yoos117@gmail.com>
2015-03-18 15:30:59 -0700

    Minor additions

--- cs480/m4/compiler.lisp
+++ cs480/m4/compiler.lisp
@@ -33,7 +33,7 @@
                       (semantics-result (semantics-parse parse-tree))
                       (dummy (format *enable-debug* "~%Semantics check: ~S~%~%" semantics-result))
                       (dummy (format *enable-debug* "~%")))
-                 (format T "~S~%~%" parse-result)
+                 (format T "~S~%" parse-result)
 
                  ;; Write to file, creating directories if necessary and overwriting preexisting files.
                  (with-open-file (ostream (ensure-directories-exist (format NIL "out/~A" arg))
